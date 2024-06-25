c4aaec138a079dd6a07a256a538d2a24e0056332
Artem <baza.a1@bk.ru>
2024-06-25 18:37:58 +0300

    Модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+class User(AbstractUser):
+    name = None
+    email = models.EmailField(unique=True, verbose_name='почта', help_text='Введите почту')
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE, help_text='Введите номер телефона')
+    avatar = models.ImageField(upload_to='users/avatars', verbose_name='аватар', **NULLABLE, help_text='Вставъте аватарку')
+    city = models.CharField(max_length=100, verbose_name='город', **NULLABLE, help_text='Введите город')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
