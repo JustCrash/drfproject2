44be200c244ee18e1838ff0f81590430195dd909
Artem <baza.a1@bk.ru>
2024-06-25 19:44:00 +0300

    Запуск сериализаторов

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+from lms.models import Course, Lesson
+from lms.serializers import CourseSerializer, LessonSerializer
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonCreateAPIView(CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveAPIView(RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyAPIView(DestroyAPIView):
+    queryset = Lesson.objects.all()
