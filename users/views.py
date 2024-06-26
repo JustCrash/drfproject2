c8b82154d35c3722efb26ab6a3709cb0c1c1a446
Artem <baza.a1@bk.ru>
2024-06-26 16:57:52 +0300

    Сериализатор для оплаты поключаем в функции

--- users/views.py
+++ users/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+
 from rest_framework.viewsets import ModelViewSet
-from users.serializers import UserSerializer
-from users.models import User
+from rest_framework.generics import ListAPIView
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
+from users.serializers import UserSerializer, PaymentSerializer
+from users.models import User, Payment
 
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class PaymentListAPIView(ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('payment_course', 'payment_lesson', 'payment_method',)
+    ordering_fields = ('payment_date',)
