3f97d5032b013140a6fe90d87bafc734498d903c
Artem <baza.a1@bk.ru>
2024-06-26 16:58:04 +0300

    Урлы для оплаты

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from users.apps import UserConfig
 from rest_framework.routers import DefaultRouter
-from users.views import UserViewSet
+from users.views import UserViewSet, PaymentListAPIView
+from django.urls import path
 
 
 app_name = UserConfig.name
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 urlpatterns = [
-
+    path('payment/', PaymentListAPIView.as_view(), name='payment')
 ] + router.urls
