c3992f8b88da2987380f8ab0f411e830447512be
Artem <baza.a1@bk.ru>
2024-06-26 15:03:35 +0300

    поключяем урлы для юзера

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from users.apps import UserConfig
+from rest_framework.routers import DefaultRouter
+from users.views import UserViewSet
 
 
 app_name = UserConfig.name
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='users')
 urlpatterns = [
 
-]
+] + router.urls
