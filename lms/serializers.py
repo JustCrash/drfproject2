da12b6bafe2437658302ec96877f52f09a964ddd
Artem <baza.a1@bk.ru>
2024-06-26 13:15:27 +0300

    1 задание 2 домашки Для модели курса добавьте в сериализатор поле вывода количества уроков. Поле реализуйте с помощью SerializerMethodField()

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,11 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from lms.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    lesson_count = SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lesson_count(self, object):
+        if object.lesson_set.count():
+            return object.lesson_set.count()
+        return 0
+
 
 class LessonSerializer(ModelSerializer):
     class Meta:
