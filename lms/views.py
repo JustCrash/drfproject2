e069af73215f9304d8b9a41306c806f452a2d621
Artem <baza.a1@bk.ru>
2024-06-30 21:22:22 +0300

    Условия

--- lms/views.py
+++ lms/views.py
@@ -1,33 +1,49 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from lms.models import Course, Lesson
 from lms.serializers import CourseSerializer, LessonSerializer
+from users.permissions import IsModerator
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'list', 'retrieve']:
+            self.permission_classes = [IsAuthenticated, IsModerator]
+        if self.action == 'create':
+            self.permission_classes = [IsAuthenticated, ~IsModerator]
+        if self.action == 'destroy':
+            self.permission_classes = [IsAuthenticated, ~IsModerator]
+        return super().get_permissions()
+
 
 class LessonCreateAPIView(CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, ~IsModerator]
 
 
 class LessonListAPIView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModerator]
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModerator]
 
 
 class LessonUpdateAPIView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModerator]
 
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated, ~IsModerator]
