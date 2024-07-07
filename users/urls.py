8e651d5290db96105530884dbc526e303758d4e4
Artem <baza.a1@bk.ru>
2024-07-07 20:20:11 +0300

    Поключил урл оплаты

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from users.apps import UserConfig
 from rest_framework.routers import DefaultRouter
 from rest_framework.permissions import AllowAny
-from users.views import UserViewSet, PaymentListAPIView
+from users.views import UserViewSet, PaymentListAPIView, PaymentCreateAPIView
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('payment/', PaymentListAPIView.as_view(), name='payment'),
     path('token/', TokenObtainPairView.as_view(permission_classes=[AllowAny]), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(permission_classes=[AllowAny]), name='token_refresh'),
+    path('payment/create/', PaymentCreateAPIView.as_view(), name='payment_create'),
 ] + router.urls
