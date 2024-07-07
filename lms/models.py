6111628767ec7c2b1e99eab2b884a44b4d020682
Artem <baza.a1@bk.ru>
2024-07-07 19:14:34 +0300

    Новое поле amount в моделях курс и урок

--- lms/models.py
+++ lms/models.py
@@ -9,6 +9,7 @@ class Course(models.Model):
     preview = models.ImageField(upload_to='course_preview/', **NULLABLE, help_text='Вставъте превью')
     description = models.TextField(max_length=250, verbose_name='описание', **NULLABLE, help_text='Введите описание')
     owner = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Владелец', **NULLABLE)
+    amount = models.PositiveIntegerField(verbose_name='Стоимость курса', **NULLABLE)
 
     def __str__(self):
         return self.title
@@ -25,6 +26,8 @@ class Lesson(models.Model):
     video = models.URLField(verbose_name='ссылка на видео', **NULLABLE, help_text='Вставте ссылку на видео')
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс', **NULLABLE, help_text='Укажите курс')
     owner = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Владелец', **NULLABLE)
+    amount = models.PositiveIntegerField(verbose_name='Стоимость урока', **NULLABLE)
+
 
     def __str__(self):
         return self.title
