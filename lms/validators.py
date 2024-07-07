0cad3edf46a3e2ae8c2ce2f364a77c8aac9eb3a2
Artem <baza.a1@bk.ru>
2024-07-07 14:19:09 +0300

    Создайте отдельный файл validators.py , реализуйте валидатор, проверяющий ссылку

--- lms/validators.py
+++ lms/validators.py
@@ -3,10 +3,10 @@ from rest_framework.exceptions import ValidationError
 
 class LinkValidator:
 
-    def __init__(self, filed):
-        self.filed = filed
+    def __init__(self, field):
+        self.field = field
 
     def __call__(self, value):
-        result = value.get(self.filed)
+        result = value.get(self.field)
         if result and not result.startswith('https://www.youtube.com/'):
             raise ValidationError('is not okay')
