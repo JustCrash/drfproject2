676b6797dab0ec8f3b5168a4a18d5db67196ad82
Artem <baza.a1@bk.ru>
2024-07-07 14:36:28 +0300

    Новая модель Subscription с полями user, course

--- lms/models.py
+++ lms/models.py
@@ -32,3 +32,15 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = 'урок'
         verbose_name_plural = 'уроки'
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс', **NULLABLE, help_text='Укажите курс')
+
+    def __str__(self):
+        return f'{self.user} - {self.course}'
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'подписки'
