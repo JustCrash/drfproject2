c8d3b65be07e8d0d04361217a694b4a6206c9480
Artem <baza.a1@bk.ru>
2024-07-17 15:00:14 +0300

    Исправил немного

--- config/celery.py
+++ config/celery.py
@@ -1,5 +1,5 @@
-from __future__ import absolute_import, unicode_literals
 import os
+
 from celery import Celery
 
 # Установка переменной окружения для настроек проекта
@@ -13,3 +13,8 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Автоматическое обнаружение и регистрация задач из файлов tasks.py в приложениях Django
 app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
