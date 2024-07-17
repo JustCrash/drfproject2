f43783202d21d28c91346b82629c505ca339a14f
Artem <baza.a1@bk.ru>
2024-07-17 19:07:08 +0300

    Подправил

--- lms/tasks.py
+++ lms/tasks.py
@@ -4,24 +4,22 @@ from django.core.mail import send_mail
 from config.settings import EMAIL_HOST_USER
 from lms.models import Subscription
 from users.models import User
+from lms.models import Course
 
 
 @shared_task
-def send_mail_about_updates(course_id):
+def send_mail_about_updates(course_id: int) -> None:
     """
     Отправка писем об обновлении курса.
     """
-    subs = Subscription.objects.filter(course=course_id)
-    subscribers_email = []
-    for sub in subs:
-        subscribers_email.append(User.objects.get(pk=sub.user.pk).email)
-        course = sub.course
-        user = sub.user
-        send_mail(
-            subject=f'{course} обновился',
-            message=f'{course} обновился',
-            from_email=EMAIL_HOST_USER,
-            recipient_list=subscribers_email,
-            fail_silently=False
-        )
-        print(f'Письмо отправлено пользователю {user.email}')
+    course = Course.objects.get(pk=course_id)
+    recipients = Subscription.objects.filter(course=course).values_list(
+        'user__email', flat=True)
+
+    send_mail(
+        subject=f'{course.title} обновился',
+        message=f'курс - {course.title} - обновился',
+        from_email=EMAIL_HOST_USER,
+        recipient_list=list(recipients),
+        fail_silently=False
+    )
